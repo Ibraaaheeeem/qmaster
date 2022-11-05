c8db6b9ee4c91fb272537ebb23a5874d730ea3e0
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-11-05 19:35:47 +0100

    Commit Project Starter

---
+++ src/test/java/com/haneef/quranmaster/QuranmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haneef.quranmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuranmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
