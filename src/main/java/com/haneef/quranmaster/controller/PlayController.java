6d0ed98db04bba3e3b85fa2760f1a3354254babe
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-11-09 22:23:50 +0100

    Add random chunck rendering for quran page

--- src/main/java/com/haneef/quranmaster/controller/PlayController.java
+++ src/main/java/com/haneef/quranmaster/controller/PlayController.java
@@ -1,5 +1,7 @@
 package com.haneef.quranmaster.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,24 @@ public class PlayController {
 
     @GetMapping("/")
     public String play(Model model){
-
+        int[] randomIndices = new int[2 * 3];
+        int limit = randomIndices.length;
+        Random r = new Random();
+        for(int i = 0; i < limit; i++){
+            randomIndices[i] = r.nextInt(limit);
+            for(int j= 0; j < i;  j++){
+                if (randomIndices[i] == randomIndices[j]) {
+                    randomIndices[j] = r.nextInt(limit);
+                }    
+            }
+            
+        }
+        for(int i = 0; i < randomIndices.length; i++){
+            System.out.println(randomIndices[i]);
+        }
         model.addAttribute("rows", new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14});
         model.addAttribute("columns", new int[]{4, 3, 2, 1, 0});
+        model.addAttribute("indices", randomIndices);
         model.addAttribute("difficulty", 5);
         return "play";
     }
