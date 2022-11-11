8e28f28b6329ba9fb3695322b6b54736d1f73c93
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-11-11 19:10:46 +0100

    Implement order states passing betweenn html and javascript

--- src/main/java/com/haneef/quranmaster/controller/PlayController.java
+++ src/main/java/com/haneef/quranmaster/controller/PlayController.java
@@ -18,20 +18,23 @@ public class PlayController {
     @GetMapping("/")
     public String play(Model model){
         List<Integer> randomIndices = new ArrayList<Integer>();
+        String randomString = "";
         int limit = 5 * 15;
         Random r = new Random();
         while (randomIndices.size() < limit){
             int randomIndex = r.nextInt(limit);
             if (!randomIndices.contains(randomIndex)){
                 randomIndices.add(randomIndex);
+                randomString += randomIndex+" ";
             }
         }
         for(int i = 0; i < randomIndices.size(); i++){
-            System.out.print(randomIndices.get(i)+" - ");
+            //System.out.print(randomIndices.get(i)+" - ");
         }
-        model.addAttribute("rows", new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14});
-        model.addAttribute("columns", new int[]{4, 3, 2, 1, 0});
+        //model.addAttribute("rows", new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14});
+        //model.addAttribute("columns", new int[]{4, 3, 2, 1, 0});
         model.addAttribute("indices", randomIndices);
+        model.addAttribute("working_indices", randomString);
         model.addAttribute("difficulty", 5);
         return "play";
     }
