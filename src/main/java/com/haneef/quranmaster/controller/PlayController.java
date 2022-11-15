e1c5a1fd53a81551eae5d6c4225e64a4e45bf0e4
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-11-15 18:25:44 +0100

    Remove colored background when a chunk is placed in its right position

--- src/main/java/com/haneef/quranmaster/controller/PlayController.java
+++ src/main/java/com/haneef/quranmaster/controller/PlayController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.haneef.quranmaster.FirstVerses;
+import com.haneef.quranmaster.Entity.FirstVerses;
 
 @Controller
 public class PlayController {
